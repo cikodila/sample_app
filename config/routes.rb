d1af76f68c3d3d39b18e961e64bdec1ded7dfbc6
cikodila <ciko.dila@teknikpadu.com.my>
2013-09-21 14:39:00 +0800

    Add precompiled assets for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "static/pages"
+  get "static/.."
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
